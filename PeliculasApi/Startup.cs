12570c0486bd328402464a1b263499fe152fcf0d
carlos melgarejo <carlos melgarejo.solorzano@hotmail.com>
2023-07-24 17:57:06 -0500

    commit

---
+++ PeliculasApi/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeliculasApi
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options
+            .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+          
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()){
+                app.UseDeveloperExceptionPage();    
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
